4cff30cfdf6bbd00ccdad4df03320dce702f6c2c
nadyashvedko <badjaaga@gmail.com>
2019-12-05 21:42:31 +0300

    add Variables homework

---
+++ Variable.java
@@ -0,0 +1,38 @@
+public class Variable{  
+
+	public static void main(String[] args){
+
+		byte core = 4;		
+		
+		short memory = 4096;
+		
+		int bitDeth = 64;
+		
+		long key  = 8992662;
+		
+		float clockSignal = 2.2f;
+		
+		double geomark = 23.849553999999998d;
+		
+		char proc = 'i';
+		
+		boolean day = false;
+			
+			System.out.println(core);
+			
+			System.out.println(memory);
+			
+			System.out.println(bitDeth);
+			
+			System.out.println(key);
+			
+			System.out.println(clockSignal);
+			
+			System.out.println(geomark);
+			
+			System.out.println(proc);
+			
+			System.out.println(day);
+
+	}
+}
