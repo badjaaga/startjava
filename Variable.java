72a880eb2db8600cb5679b6a2d981dedb8ee822b
nadyashvedko <badjaaga@gmail.com>
2019-12-05 21:54:38 +0300

    refactor variables

--- Variable.java
+++ Variable.java
@@ -1,38 +1,21 @@
 public class Variable{  
-
 	public static void main(String[] args){
-
-		byte core = 4;		
-		
-		short memory = 4096;
-		
+		byte coreCount = 4;		
+		short memoryAmount = 4096;
 		int bitDeth = 64;
-		
-		long key  = 8992662;
-		
+		long key  = 8992662L;
 		float clockSignal = 2.2f;
-		
 		double geomark = 23.849553999999998d;
-		
 		char proc = 'i';
+		boolean isPowerOn = false;
 		
-		boolean day = false;
-			
-			System.out.println(core);
-			
-			System.out.println(memory);
-			
-			System.out.println(bitDeth);
-			
-			System.out.println(key);
-			
-			System.out.println(clockSignal);
-			
-			System.out.println(geomark);
-			
-			System.out.println(proc);
-			
-			System.out.println(day);
-
+		System.out.println(coreCount);
+		System.out.println(memoryAmount);
+		System.out.println(bitDeth);
+		System.out.println(key);
+		System.out.println(clockSignal);
+		System.out.println(geomark);
+		System.out.println(proc);
+		System.out.println(isPowerOn);
 	}
 }
