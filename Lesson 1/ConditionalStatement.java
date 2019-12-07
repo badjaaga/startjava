657f850f61baaba7121a458382a5f51116010592
nadyashvedko <badjaaga@gmail.com>
2019-12-07 21:56:03 +0300

    add conditional statements homework

---
+++ Lesson 1/ConditionalStatement.java
@@ -0,0 +1,29 @@
+public class ConditionalStatement {
+	public static void main(String[] args){
+		int age = 30;
+		if(age > 20) {
+			System.out.println("You are lucky");
+		}
+		boolean isMale = true;
+		if(isMale) {
+			System.out.println("Congratulations!");
+		}
+		if(!isMale) {
+			System.out.println("Are you sure?");
+		}
+		double height = 1.90;
+		if(height < 1.80) {
+			System.out.println("Congratulations!");
+		} else {
+			System.out.println("Are you sure?");
+		}
+		char firstLetterOfTheName = 'M';
+		if(firstLetterOfTheName == 'M') {
+			System.out.println("The first letter of your name is M");
+		} else if(firstLetterOfTheName == 'I'){
+			System.out.println("The first letter of your name is N");
+		} else {
+			System.out.println("The first letter of your name is not M and not N");
+		}
+	}
+}
