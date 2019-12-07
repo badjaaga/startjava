774c3fe299d5cc4cd309a2abacee8ee8d0946a48
nadyashvedko <badjaaga@gmail.com>
2019-12-07 19:45:48 +0300

    add changes according to code convention

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {  
+	public static void main(String[] args) {
+		byte coreCount = 4;		
+		short memoryAmount = 4096;
+		int bitDeth = 64;
+		long key = 8992662L;
+		float clockSignal = 2.2f;
+		double geomark = 23.849553999999998d;
+		char proc = 'i';
+		boolean isPowerOn = false;
+		
+		System.out.println(coreCount);
+		System.out.println(memoryAmount);
+		System.out.println(bitDeth);
+		System.out.println(key);
+		System.out.println(clockSignal);
+		System.out.println(geomark);
+		System.out.println(proc);
+		System.out.println(isPowerOn);
+	}
+}
