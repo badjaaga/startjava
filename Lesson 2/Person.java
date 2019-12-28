184a43515af696bc89749a9380935b071108b0db
Nadya Shvedko <badjaaga@gmail.com>
2019-12-28 14:01:05 +0300

    add Person class

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	char gender = 'm';
+	String name = "Vasya";
+	short height = 185;
+	short weight = 80;
+	short age = 40;
+
+	void run() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void go() {
+
+	}
+
+	void speak() {
+
+	}
+
+	void learnJava() {
+		
+	}
+}
